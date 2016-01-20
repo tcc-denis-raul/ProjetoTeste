70fede6f2d39071c1a66fdb14d218829f408d81e
Denis Aoki <dnsaoki@a.local>
2016-01-20 17:03:07 -0200

    teste

--- Script/Conectar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$base_nome      = "paloma";
-$colecao_cursos = "cursos_idiomas";
-
-$mongo_cliente = new MongoClient();
-$db            = $mongo_cliente->$base_nome;
--- Script/cotacao_dolar.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!$fp = fopen("http://dolarhoje.com/cotacao.txt", "r")) {
-    echo "Erro ao abrir a página de cotação";
-    exit;
-}
-$dolar = str_replace(",", ".", fgets($fp));
-fclose($fp);
